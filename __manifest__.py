7b9201a1d4ece09915a17b99cb97a622c40a3771
ELfassiMohamed <elfassimohamed2001@gmail.com>
2025-08-18 16:32:14 +0100

    first commit : before starting to code

---
+++ __manifest__.py
@@ -0,0 +1,9 @@
+{
+    'name': 'Interventions',
+    'version': '17.0.1.0.0',
+    'category': '',
+    'author': 'EL FASSI Mohamed',
+    'depends': ['base',],
+    'data': [],
+    'application': True,
+}
